80d29f3aa2c6330f13ede2d1e3e83db956223b83
Hun <adeliae.p1841@gmail.com>
2025-02-23 21:07:28 +0900

    [Solved] BOJ3621 문제풀이

---
+++ 202502_3/BOJ3621.py
@@ -0,0 +1,29 @@
+n, d = map(int, input().split())
+
+family = list(map(int, input().split()))
+adj_list = [[] * (n + 1) for _ in range(n + 1)]
+
+for idx, father in enumerate(family):
+    adj_list[father].append(idx)
+
+def how_many_add(number_father, limit):
+    additional = 0
+    if number_father > limit:
+        remain = number_father // limit
+        if remain > limit:
+            additional += how_many_add(remain, limit) + how_many_add(number_father - remain, limit)
+            additional += limit
+        else:
+            additional += remain
+    return additional
+
+
+additional = 0
+for row in adj_list:
+    additional += how_many_add(len(row), d)
+    #print(len(row))
+
+print(additional)
+
+
+
