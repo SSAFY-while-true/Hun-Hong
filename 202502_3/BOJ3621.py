753d6a0b4145123776d0e10193fc450d5ed5e155
Hun <adeliae.p1841@gmail.com>
2025-02-27 20:01:55 +0900

    [DOC] '족보' 문제 주석 추가

--- 202502_3/BOJ3621.py
+++ 202502_3/BOJ3621.py
@@ -6,6 +6,10 @@ adj_list = [[] * (n + 1) for _ in range(n + 1)]
 for idx, father in enumerate(family):
     adj_list[father].append(idx)
 
+# 재귀를 이용하여 반복적으로 선조를 할당하는 함수
+# ex)
+# 7 2
+# 0 0 0 0 0 0 0
 def how_many_add(number_father, limit):
     additional = 0
     if number_father > limit:
