ec905e215d52588e25ff57d7dd07df70878dcb8c
Hun <adeliae.p1841@gmail.com>
2025-03-02 18:14:09 +0900

    [Solved] '동물원' 문제 풀이

---
+++ 202503_1/BOJ1309.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+
+if __name__ == "__main__":
+    # 열에 사자를 배치하지 않음  :0
+    # 열 위에 사자를 배치        :1
+    # 열 아래에 사자를 배치      :2
+    # 상태는 총 3개
+    N = int(input())
+    dp = [[1] * 2 for _ in range(3)]
+    dp[0][0] = 1
+    dp[1][0] = 1
+    dp[2][0] = 1
+    for _ in range(0, N - 1):
+        dp[0][1] = dp[0][0] + dp[1][0] + dp[2][0]
+        dp[1][1] = dp[0][0] + dp[2][0]
+        dp[2][1] = dp[0][0] + dp[1][0]
+
+        dp[0][0] = dp[0][1]
+        dp[1][0] = dp[1][1]
+        dp[2][0] = dp[2][1]
+
+    print(sum([dp[i][1] for i in range(3)]) % 9901)
