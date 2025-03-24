be33636d164cafcbb3fbee8f05928366c3c862a8
Hun <adeliae.p1841@gmail.com>
2025-03-24 12:00:31 +0900

    [Solved] '적록색약' 문제 풀이

--- 202503_4/BOJ10026.py
+++ 202503_4/BOJ10026.py
@@ -1,7 +1,9 @@
+import copy
+
 def find_start(matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix)):
-            if matrix[i][j] != 0:
+            if matrix[i][j] != '0':
                 return (i, j)
 
 
@@ -10,20 +12,34 @@ def dfs(start, matrix, color):
 
     while stack:
         curr_i, curr_j = stack.pop()
-        matrix[curr_i][curr_j] = 0
+        matrix[curr_i][curr_j] = '0'
         for d_i, d_j in move_type:
             next_i, next_j = curr_i + d_i, curr_j + d_j
-            if matrix[next_i][next_j] in color:
-                stack.append((next_i, next_j))
-
+            if 0 <= next_i < N and 0 <= next_j < N:
+                if matrix[next_i][next_j] in color:
+                    stack.append((next_i, next_j))
+    return matrix
 
 N = int(input())
 
 move_type = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 art = [list(input()) for _ in range(N)]
+n_count = rg_count = 0
+n_art = copy.deepcopy(art)
+rg_art = copy.deepcopy(art)
 
-while True:
-    start = find_start(art)
-    color = art[start[0]][start[1]]
+while find_start(n_art):
+    start = find_start(n_art)
+    color = n_art[start[0]][start[1]]
+    n_count += 1
+    n_art = dfs(start, n_art, [color])
 
-    
+while find_start(rg_art):
+    start = find_start(rg_art)
+    color = rg_art[start[0]][start[1]]
+    rg_count += 1
+    if color in ['R', 'G']:
+        color = ['R', 'G']
+    rg_art = dfs(start, rg_art, color)
+
+print(n_count, rg_count)
--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 ---
 
-- **[BOJ:10026]** [적록색약](https://www.acmicpc.net/problem/10026)
+- **[BOJ:10026]** [적록색약](https://www.acmicpc.net/problem/10026) **[풀이 완료]**
 - **[BOJ:1074]** [Z](https://www.acmicpc.net/problem/1074)
 - **[BOJ:14503]** [로봇 청소기](https://www.acmicpc.net/problem/14503)
 - **[PRO:150368]** [이모티콘 할인행사](https://school.programmers.co.kr/learn/courses/30/lessons/150368) **[풀이 완료]**
