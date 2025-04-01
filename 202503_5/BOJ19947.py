97a8d79f5b77d29df2c922628f28234df492be96
Hun <adeliae.p1841@gmail.com>
2025-04-01 22:39:57 +0900

    [Solved] '투자의 귀재 배주형' 문제 풀이

---
+++ 202503_5/BOJ19947.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    H, Y = map(int, input().split())
+
+    dp = [0] * (Y + 1)
+
+    dp[0] = H
+
+    for i in range(1, Y + 1):
+        if i >= 5:
+            dp[i] = int(max(dp[i - 1] * 1.05, dp[i - 3] * 1.2, dp[i - 5] * 1.35))
+        elif i >= 3:
+            dp[i] = int(max(dp[i - 1] * 1.05, dp[i - 3] * 1.2))
+        else:
+            dp[i] = int(dp[i - 1] * 1.05)
+
+    print(int(dp[Y]))
